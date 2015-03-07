7dd62241745c82af9400766d468fb4226d29fd29
elertan <99027784@mydavinci.nl>
2015-03-07 13:40:46 +0100

    Test

--- LeagueExtender/ExternalFeatures.cs
+++ LeagueExtender/ExternalFeatures.cs
@@ -21,6 +21,7 @@ namespace LeagueExtender
             public int Top;         // y position of upper-left corner
             public int Right;       // x position of lower-right corner
             public int Bottom;      // y position of lower-right corner
+            public int Middle;
             public int Width
             {
                 get
