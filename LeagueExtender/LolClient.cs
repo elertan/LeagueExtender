87d28eed55eafc86067a5688ce1652d2cb0e8a7d
elertan <99027784@mydavinci.nl>
2015-03-07 16:36:15 +0100

    Added new Features

---
+++ LeagueExtender/LolClient.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace LeagueExtender
+{
+    public class LolClient : IWin32Window
+    {
+
+        private ExternalFeatures.RECT _RECT;
+        private Size _OrginalSize;
+
+        public event EventHandler MovedWindow;
+        public event EventHandler MinimizedWindow;
+
+        private void OnMovedWindow()
+        {
+            if (MovedWindow != null)
+            {
+                MovedWindow(this, EventArgs.Empty);
+            }
+        }
+        
+        private void OnMinimizedWindow()
+        {
+            if (MinimizedWindow != null)
+            {
+                MinimizedWindow(this, EventArgs.Empty);
+            }
+        }
+
+        public IntPtr Handle { get; set; }
+
+        /// <summary>
+        /// The Size of the window, avoid setting this property since the window won't resize its controls.
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                this._RECT = this.GetRect();
+                if (this._RECT.Top != -32000)
+                {
+                    return new Size(this._RECT.Right - this._RECT.Left, this._RECT.Bottom - this._RECT.Top);
+                }
+                throw new Exception("Couldn't retrieve the Size");
+            }
+            set
+            {
+                ExternalFeatures.SetWindowPos(this.Handle, IntPtr.Zero, this.Location.X, this.Location.Y, value.Width, value.Height, ExternalFeatures.SetWindowPosFlags.SWP_ASYNCWINDOWPOS);
+            }
+        }
+
+        public Size OriginalSize
+        {
+            get
+            {
+                return this._OrginalSize;
+            }
+        }
+
+        public Point Location
+        {
+            get
+            {
+                this._RECT = this.GetRect();
+                if (this._RECT.Top != -32000)
+                {
+                    return new Point(this._RECT.Left, this._RECT.Top);
+                }
+                throw new Exception("Couldn't retrieve the Location");
+            }
+        }
+
+        public bool IsMinimized
+        {
+            get
+            {
+                return this.GetRect().Top == -32000;
+            }
+        }
+
+        public bool IsAlive
+        {
+            get
+            {
+                return Process.GetProcessesByName("LolClient").Length > 0;
+            }
+        }
+
+        public LolClient()
+        {
+            Process[] procs = Process.GetProcessesByName("LolClient");
+            if (procs.Length == 0)
+            {
+                throw new Exception("LolClient not found!");
+            }
+            this.Handle = procs[0].MainWindowHandle;
+
+            this._OrginalSize = this.GetSize(true);
+        }
+
+        //public LolClient(IntPtr Handle)
+        //{
+        //    this.Handle = Handle;
+        //    this._OrginalSize = this.GetSize(true);
+        //}
+
+
+        
+
+        /// <summary>
+        /// Returns the size of the window
+        /// </summary>
+        /// <param name="Force">Forces to get a result (Actives window if wasnt activated before)</param>
+        /// <returns></returns>
+        private Size GetSize(bool Force = false)
+        {
+            if (Force)
+            {
+                ExternalFeatures.SetForegroundWindow(this.Handle);
+            }
+            ExternalFeatures.RECT rcSize = this.GetRect();
+            if (rcSize.Top != -32000)
+            {
+                return new Size(rcSize.Right - rcSize.Left, rcSize.Bottom - rcSize.Top);
+            }
+            return new Size(0, 0);
+        }
+
+        private ExternalFeatures.RECT GetRect()
+        {
+            ExternalFeatures.RECT rcSize;
+            ExternalFeatures.GetWindowRect(this.Handle, out rcSize);
+            return rcSize;
+        }
+
+    }
+}
--- LeagueExtender/MainWindow.cs
+++ LeagueExtender/MainWindow.cs
@@ -10,9 +10,51 @@ namespace LeagueExtender
 {
     public partial class MainWindow : Form
     {
+
+        LolClient LolClient;
+
+        ~MainWindow() // For stopping a movement task later
+        {
+            
+        }
+
         public MainWindow()
+        {
+            this.LolClient = new LolClient();
+
+            //this.ShowInTaskbar = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            //this.TransparencyKey = Color.Yellow;
+            this.BackColor = Color.Yellow;
+
+            this.Load += MainWindow_Load;
+
+            this.LolClient.MovedWindow += LolClient_MovedWindow;
+            this.LolClient.MinimizedWindow += LolClient_MinimizedWindow;
+        }
+
+        void LolClient_MinimizedWindow(object sender, EventArgs e)
         {
 
         }
+
+        void LolClient_MovedWindow(object sender, EventArgs e)
+        {
+
+        }
+
+        void MainWindow_Load(object sender, EventArgs e)
+        {
+            this.MoveToFront();
+            this.Location = this.LolClient.Location;
+            Button b = new Button { BackColor = Color.Transparent, Text = "Test" };
+            this.Controls.Add(b);
+        }
+
+        private void MoveToFront()
+        {
+            ExternalFeatures.SetForegroundWindow(this.Handle);
+        }
+       
     }
 }
